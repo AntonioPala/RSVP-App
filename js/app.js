f9632c1b7f7a9ca86e734b1f78dc717fe6a46df4
AntonioPala <37319493+AntonioPala@users.noreply.github.com>
2020-05-20 14:49:26 -0400

    Update app.js
    Added checkbox and functionality

--- js/app.js
+++ js/app.js
@@ -9,5 +9,23 @@ form.addEventListener('submit', (e) => {
   const text = input.value;
   input.value = "";
   li.textContent = text;
+  const label = document.createElement('label');
+  label.textContent = 'Confirmed';
+  const checkbox = document.createElement('input');
+  checkbox.type = 'checkbox';
+  label.appendChild(checkbox);
+  li.appendChild(label);
   ul.appendChild(li);
 })
+
+ul.addEventListener('change', (e) => {
+  const checkbox = e.target;
+  const checked = checkbox.checked;
+  const listItem = checkbox.parentNode.parentNode;
+
+  if (checked) {
+    listItem.className = 'responded';
+  } else {
+      listItem.className = '';
+    }
+});
